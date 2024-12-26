6c76281ccf0daf1bccba64bc13ed785e2d8a0d96
like-coder <81460494+like-coder@users.noreply.github.com>
2024-12-26 23:46:47 +0800

    create a new learn gas project

---
+++ Source/LearnGAS/LearnGAS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LearnGAS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LearnGAS, "LearnGAS" );
